b72d603333b4f3a6f8928fa5b4487ea86e3b431e
sansuke05 <takanori5064@gmail.com>
2022-02-20 05:19:25 +0900

    implement avatar osc prototype

--- trigger_selector.py
+++ trigger_selector.py
@@ -4,20 +4,20 @@ import trigger_data
 from utility import *
 
 triggers = [
-    { 
+    {
         'address': '/cube/activate',
-        'キューブ アクティブ':1,
-        'キューブ オフ':0 
+        'キューブ アクティブ': 1,
+        'キューブ オフ': 0
     },
-    { 
+    {
         'address': '/magicparticle/activate',
-        '詠唱 開始':1,
-        '詠唱 終了':0
+        '詠唱 開始': 1,
+        '詠唱 終了': 0
     },
     {
         'address': '/light/activate',
-        '電気 つけて':1,
-        '電気 消して':0
+        '電気 つけて': 1,
+        '電気 消して': 0
     }
 ]
 
@@ -40,6 +40,15 @@ def is_auto_off(trigger):
 # トリガーキーと入力文字をパターンマッチさせる
 def select(input_text):
     #global triggers
+    data = trigger_data.data_in_current_world
+    type_str = 'type'
+    if type_str in data and data[type_str] == 'Avatar':
+        return select_avatar(input_text)
+    else:
+        return select_world(input_text)
+
+
+def select_world(input_text):
     address = ''
     auto_off = False
 
@@ -55,7 +64,7 @@ def select(input_text):
             for p in key_parts:
                 pattern += r'(?=.*{0})'.format(p)
             pattern += r'.*$'
-            #print(pattern)
+            # print(pattern)
 
             # パターンマッチ
             m = re.search(pattern, input_text)
@@ -67,8 +76,37 @@ def select(input_text):
                     auto_off = True
                     return (address, value, auto_off)
                 return (address, value, auto_off)
-    #[loop END]
+    # [loop END]
     return ('', -1, auto_off)
+
+
+def select_avatar(input_text):
+    address = ''
+
+    for trigger_tmp in trigger_data.data_in_current_world['OSCButtonTrigers']:
+        trigger = dict(trigger_tmp)
+        address = trigger.pop('address')
+        if trigger.pop('type') != 'int':
+            continue
+        trigger_keys = trigger.pop('changeTriggerKeys')
+
+        for index, trigger_key in enumerate(trigger_keys):
+            pattern = r'^'
+            key_parts = trigger_key.split(' ')
+
+            # 正規表現の作成
+            for p in key_parts:
+                pattern += r'(?=.*{0})'.format(p)
+            pattern += r'.*$'
+            # print(pattern)
+
+            # パターンマッチ
+            m = re.search(pattern, input_text)
+
+            if m:
+                return (address, index, False)
+    # [loop END]
+    return ('', -1, False)
 
 
 # 単体テスト用
@@ -91,4 +129,4 @@ if __name__ == "__main__":
 
     for txt in texts:
         print(select(txt))
-        #print(trigger_data.data_in_current_world)
+        # print(trigger_data.data_in_current_world)
